07906072164f22b95368fe0406e0b42339770729
Miguel Ben <codexheartdevs@gmail.com>
2019-11-25 04:25:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, idx)
-  if idx.beetwen?(1, 9) && !position_taken
+  if idx.beetwen?(1, 9) && !position_taken?(board, idx)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
