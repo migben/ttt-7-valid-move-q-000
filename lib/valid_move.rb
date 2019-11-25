a24ad6c256d57baab192fda5587fe8d0c9bd37af
Miguel Ben <codexheartdevs@gmail.com>
2019-11-25 04:28:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, idx)
-  if idx.between?(1, 8) && !position_taken?(board, idx)
+  if idx.between?(0, 8) && !position_taken?(board, idx)
     return true
   end
 end
